ac74ad6e7cf5df722ae24c161ccf85e9a6e2cfd5
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-08-23 16:52:15 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -8,9 +8,9 @@ from ..logging import LOGGER
 
 class JARVIS(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"MUSICBROKN is on the way...")
+        LOGGER(__name__).info(f"ANJALIMUSIC is on the way...")
         super().__init__(
-            name="MUSICBROKN",
+            name="ANJALIMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
