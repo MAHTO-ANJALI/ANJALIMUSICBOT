bc560be3590af50efbfa41b42ddbfd7df6458d2b
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-08-23 22:39:09 +0530

    Update search.py

--- ANNIEMUSIC/plugins/tools/search.py
+++ ANNIEMUSIC/plugins/tools/search.py
@@ -123,7 +123,7 @@ async def search_(app: app, msg: Message):
         return
     except Exception as e:
         await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @JARVIS_V2")
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @AnjaliOwnerBot")
         print(f"error : {e}")
         return
 
@@ -192,6 +192,6 @@ async def stack_search_(app: app, msg: Message):
         return
     except Exception as e:
         await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @JARVIS_V2")
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @AnjaliOwnerBot")
         print(f"error : {e}")
         return
