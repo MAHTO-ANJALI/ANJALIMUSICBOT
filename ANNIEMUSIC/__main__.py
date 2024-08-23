dbf435bf9615c75ac6aa6e0b62a2093b62c9fd05
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-08-23 22:26:23 +0530

    Update __main__.py

--- ANNIEMUSIC/__main__.py
+++ ANNIEMUSIC/__main__.py
@@ -42,20 +42,20 @@ async def init():
     try:
         await JARVIS.stream_call("https://te.legra.ph/file/774624c731c7b51b09a6b.mp4")
     except NoActiveGroupCall:
-        LOGGER("MUSICBROKN").error(
+        LOGGER("ANJALIMUSIC").error(
             "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ.\n\nᴀɴɴɪᴇ ʙᴏᴛ sᴛᴏᴘᴘᴇᴅ..."
         )
         exit()
     except:
         pass
     await JARVIS.decorators()
-    LOGGER("MUSICBROKN").info(
+    LOGGER("ANJALIMUSIC").info(
         "\x41\x6e\x6e\x69\x65\x20\x4d\x75\x73\x69\x63\x20\x52\x6f\x62\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x2e\x2e"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("MUSICBROKN").info("sᴛᴏᴘɪɴɢ ᴀɴᴊᴀʟɪ ᴍᴜsɪᴄ ʙᴏᴛ ...")
+    LOGGER("ANJALIMUSIC").info("sᴛᴏᴘɪɴɢ ᴀɴᴊᴀʟɪ ᴍᴜsɪᴄ ʙᴏᴛ ...")
 
 
 if __name__ == "__main__":
